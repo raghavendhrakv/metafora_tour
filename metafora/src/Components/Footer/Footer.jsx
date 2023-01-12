19b53216135ac4bf39dd066e58f8ca579c1c4a17
raghavendhrakv <83531560+raghavendhrakv@users.noreply.github.com>
2023-01-12 10:34:05 +0530

    Updated Footer.jsx
    - imported and used Aos 
    - using fade-up effect in the footer content
    -

--- metafora/src/Components/Footer/Footer.jsx
+++ metafora/src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './footer.scss'
+import React,{useEffect} from 'react'
+import './footer.css'
 import video2 from '../../Assets/video.mp4'
 import {FiSend} from 'react-icons/fi'
 import {SiYourtraveldottv} from 'react-icons/si'
@@ -9,132 +9,141 @@ import {AiFillInstagram} from 'react-icons/ai'
 import {FaTripadvisor} from 'react-icons/fa'
 import {FiChevronRight} from 'react-icons/fi'
 
+import Aos from 'aos'
+import 'aos/dist/aos.css' 
+
 const Footer = () =>{
-return(
-<section className='footer'>
-<div className="videoDiv">
-<video src={video2} loop autoPlay muted type="video/mp4"></video>
-</div>
+    useEffect(()=>{
+        Aos.init({duration: 2000})
+      },[])
+    return(
+        <section className='footer'>
+            <div className="videoDiv">
+                <video src={video2} loop autoPlay muted type="video/mp4"></video>
+            </div>
 
-<div className="secContent container">
-<div className="contactDiv flex">
-<div className="text">
-<small>KEEP IN TOUCH</small>
-<h2>Travel-with-us</h2>
-</div>
+            <div className="secContent container">
+                <div className="contactDiv flex">
+                    <div data-aos="fade-up" className="text">
+                        <small>KEEP IN TOUCH</small>
+                        <h2>Travel-with-us</h2>
+                    </div>
+                    <div className='inputDiv flex'>
+                        <input data-aos="fade-up" type="text" placeholder='Enter Email Address'/>
+                        <button data-aos="fade-up" className='btn flex' type='submit'>
+                            SEND<FiSend className="icon"/>
+                        </button>
+                    </div>
+                </div>
+                <div className="footerCard flex">
+                    <div className="footerIntro flex">
+                        <div className="logoDiv">
+                            <a href="#" className='logo flex'>
+                                <SiYourtraveldottv className="icon"/> Travel.
+                            </a>
+                        </div>
+                        <div data-aos="fade-up" className="footerParagraph">
+                        </div>
+                        <div data-aos="fade-up" className="footerSocials flex">
+                            <AiOutlineTwitter className="icon"/>
+                            <AiFillYoutube className="icon"/>
+                            <AiFillInstagram className="icon"/>
 
-<div className="footerCard flex">
-<div className="footerIntro flex">
-<div className="logoDiv">
-<a href="#" className='logo flex'>
-<SiYourtraveldottv className="icon"/> Travel.
-</a>
-</div>
+                            <FaTripadvisor className="icon"/>
+                        </div>
+                    </div>
 
-<div className="footerSocials flex">
-<AiOutlineTwitter className="icon"/>
-<AiFillYoutube className="icon"/>
-<AiFillInstagram className="icon"/>
+                    <div className="footerLinks grid">
+                        <div data-aos="fade-up" data-aos-duration="3000" className='linkGroup'>
+                            <span className='groupTitle'>
+                                OUR AGENCY
+                            </span>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                Services
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                Insurance
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                Agency
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                Tourism
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                Payment
+                            </li>
+                        </div>
+                        <div data-aos="fade-up" data-aos-duration="4000" className='linkGroup'>
+                            <span className='groupTitle'>
+                            PARTNERS
+                            </span>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                Bookings
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                RentCars
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                HostelWorld
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                Trivago
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                TripAdvisor 
+                            </li>
+                        </div> 
+                        <div data-aos="fade-up" data-aos-duration="4000" className='linkGroup'>
+                            <span className='groupTitle'>
+                            LAST MINUTE
+                            </span>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                London
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                California
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                Indonesia
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                Europe
+                            </li>
+                            <li className='footerList flex'>
+                                <FiChevronRight className="icon"/>
+                                Oceania
+                            </li>
+                        </div> 
+                    </div>
+                    <div className='footerDiv flex'>
+                        <small>BEST TRAVEL WEBSITE </small>
+                        <small>COPYRIGHT RESERVED @MeTaFoRa_23 </small>
+                    </div>
+                </div>
+            </div>
+            <div className="inputDiv flex">
+                <input type="text" placeholder='Enter Email Address'/>
+                    <button className='btn flex' type='submit'>
+                    SEND <FiSend className="icon"/>
+                    </button>
+            </div>
 
-<FaTripadvisor className="icon"/>
-</div>
-<div className="footerParagraph">
-hello
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-</div>
-</div>
-
-<div className="footerLinks grid">
-    <div className='linkGroup'>
-        <span className='groupTitle'>
-            OUR AGENCY
-        </span>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            Services
-        </li>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            Insurance
-        </li>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            Agency
-        </li>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            Tourism
-        </li>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            Payment
-        </li>
-    </div>
-    <div className='linkGroup'>
-        <span className='groupTitle'>
-        PARTNERS
-        </span>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            Bookings
-        </li>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            RentCars
-        </li>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            HostelWorld
-        </li>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            Trivago
-        </li>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            TripAdvisor 
-        </li>
-    </div> 
-    <div className='linkGroup'>
-        <span className='groupTitle'>
-           LAST MINUTE
-        </span>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            London
-        </li>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            California
-        </li>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            Indonesia
-        </li>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            Europe
-        </li>
-        <li className='footerList flex'>
-            <FiChevronRight className="icon"/>
-            Oceania
-        </li>
-    </div> 
-</div>
-<div className='footerDiv flex'>
-<small>BEST TRAVEL WEBSITE </small>
-<small>COPYRIGHT RESERVED @MeTaFoRa_23 </small>
-</div>
-</div>
-</div>
-<div className="inputDiv flex">
-<input type="text" placeholder='Enter Email Address'/>
-<button className='btn flex' type='submit'>
-SEND <FiSend className="icon"/>
-</button>
-</div>
-</div>
-</section>
-)
+        </section>
+    )
 }
 export default Footer
